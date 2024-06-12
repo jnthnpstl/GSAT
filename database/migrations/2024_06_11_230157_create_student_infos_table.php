22214c03460f4e1c4000427216435d0b7f0146b3
jnthnpstl <jnthnpstl@gmail.com>
2024-06-12 08:19:48 +0800

    foreign keys added

--- database/migrations/2024_06_11_230157_create_student_infos_table.php
+++ database/migrations/2024_06_11_230157_create_student_infos_table.php
@@ -15,6 +15,26 @@ class CreateStudentInfosTable extends Migration
     {
         Schema::create('student_infos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('lname', 100);
+            $table->string('fname', 100);
+            $table->string('mname', 100);
+            $table->string('pob', 100);
+            $table->date('dob');
+            $table->string('citizenship', 100);
+            $table->integer('sex');
+            $table->string('civilstatus', 100);
+            $table->string('home_address', 100);
+            $table->string('contact_number', 50);
+            $table->string('email', 100);
+            $table->string('degree_program_applied', 100);
+            $table->string('major', 100);
+            $table->string('apply_for_semester', 100);
+            $table->string('prev_applied', 100);
+            $table->string('publications', 100);
+            $table->string('unpublished', 100);
+            $table->string('future_plans', 100);
             $table->timestamps();
         });
     }
--- database/migrations/2024_06_11_230446_create_programchair_per_degrees_table.php
+++ database/migrations/2024_06_11_230446_create_programchair_per_degrees_table.php
@@ -15,6 +15,8 @@ class CreateProgramchairPerDegreesTable extends Migration
     {
         Schema::create('programchair_per_degrees', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
